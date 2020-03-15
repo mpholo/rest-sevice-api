49251da29c644d848637f92b5f0ef05c8bf273cb
Mpholo Leboea <mpholo.leboea@gmail.com>
2020-03-15 22:53:20 +0200

    added swaggerui

--- src/main/java/com/learning/JAXRSConfiguration.java
+++ src/main/java/com/learning/JAXRSConfiguration.java
@@ -5,6 +5,7 @@ import io.swagger.jaxrs.config.BeanConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+//http://localhost:8080/rest-service-api/rest/swagger.json
 
 @ApplicationPath("/rest")
 public class JAXRSConfiguration extends Application {
@@ -15,10 +16,11 @@ public class JAXRSConfiguration extends Application {
         beanConfig.setVersion("2.0.0");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
-        beanConfig.setBasePath("/rest-servce-api/rest");
+        beanConfig.setBasePath("/rest-service-api/rest");
         beanConfig.setResourcePackage("com.learning");
         beanConfig.setPrettyPrint(true);
         beanConfig.setScan();
+
         //swagger JAX-RS JSON generation ENDS
     }
 
